736e46db41d14380305724305c4875e74bda6488
Juan Felipe Bojorges <bluespheal@gmail.com>
2021-05-08 18:08:35 -0500

    Set random enemy locations in levels
    - Set random enemy locations in levels
    - Make levels loop

---
+++ Assets/Level_enemy_spawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_enemy_spawner : MonoBehaviour
+{
+    public GameObject enemy_array_1;
+    public GameObject enemy_array_2;
+
+    public GameObject[] formation_array;
+
+    private int selected_array;
+    // Start is called before the first frame update
+    void Start()
+    {
+        formation_array[0] = enemy_array_1;
+        formation_array[1] = enemy_array_2;
+
+        foreach (GameObject enemy_group in formation_array)
+        {
+            enemy_group.active = false;
+        }
+
+        selected_array = Random.Range(0, 2);
+        formation_array[selected_array].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
